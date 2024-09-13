aa2e539a627313f518dbe52196055f1089522fc9
boran <borangoher@proton.me>
2024-09-13 16:16:04 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# nvim
+mkdir -p "$HOME/.config/nvim"
+mkdir -p "$HOME/.config/nvim/undo"
+ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
+
+# X11
+rm -rf "$HOME/.config/X11"
+ls -s "$HOME/dotfiles/X11" "$HOME/.config"
+
